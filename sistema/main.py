870e61c8aca96295c832e44fb5601e52aac5bcd9
Eduardo Ramos <otr07eduardo@gmail.com>
2026-09-26 19:18:13 -0300

    Adiciona ultimas correcoes
    Co-authored-by: FelipeAndriFe <faferreira@inf.ufpel.edu.br>

--- sistema/main.py
+++ sistema/main.py
@@ -1,159 +1,7 @@
-from queue import PriorityQueue
 import requests
+from estruturas.trie import Trie
+from estruturas.huffman import encode, get_huffman_data
 
-# HUFFMAN CODE
-encode_map = {}
-freq_map = {}
-huffman_post_list = []
-huffman_in_list = []
-
-class HuffmanNode:
-    def __init__(self, freq, data, left, right):
-        self.freq = freq
-        self.data = data
-        self.left = left
-        self.right = right
-
-    def __lt__(self, other):
-        return self.freq < other.freq
-
-def count_freq(message):
-    for char in message:
-        if char in freq_map.keys():
-            freq_map[char] += 1
-        else:
-            freq_map[char] = 1
-
-def create_huffman_tree():
-    p_queue = PriorityQueue()
-
-    for key in freq_map.keys():
-        node = HuffmanNode(freq_map[key], key, None, None)
-        p_queue.put((node.freq, node))
-
-    while p_queue.qsize() > 1:
-        _, first = p_queue.get()
-        _, second = p_queue.get()
-        parent_freq = first.freq + second.freq
-
-        parent_node = HuffmanNode(parent_freq, str(parent_freq), first, second)
-        p_queue.put((parent_node.freq, parent_node))
-
-    _, root = p_queue.get()
-    return root
-
-def set_bit_code(node, bit_str):
-    if node is None:
-        return
-    if node.left is None and node.right is None:
-        if bit_str == "":
-            bit_str = "0"
-
-        encode_map[node.data] = bit_str
-        return
-
-    set_bit_code(node.left, bit_str + "0")
-    set_bit_code(node.right, bit_str + "1")
-
-def post_in_traversal(node):
-    if node is None:
-        return
-    post_in_traversal(node.left)
-    huffman_in_list.append(node.data)
-    post_in_traversal(node.right)
-    huffman_post_list.append(node.data)
-
-def encode(message):
-    global encode_map
-    global freq_map
-    global huffman_post_list
-    global huffman_in_list
-    encode_map = {}
-    freq_map = {}
-    huffman_post_list = []
-    huffman_in_list = []
-
-    if not message:
-        return ""
-
-    count_freq(message)
-    root = create_huffman_tree()
-    post_in_traversal(root)
-    set_bit_code(root, "")
-
-    return "".join(encode_map[char] for char in message)
-
-# TRIE CODE
-class TrieNode:
-    def __init__(self):
-        # Cada índice representa um caractere ASCII, evitando o uso de dict
-        self.children = [None] * 256
-        self.is_end_of_word = False
-        self.original_word = None
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def get_index(self, char):
-        return ord(char)
-
-    def insert(self, word):
-        original_word = word
-        word = word.lower()
-        current_node = self.root
-
-        for char in word:
-            index = self.get_index(char)
-
-            if current_node.children[index] is None:
-                current_node.children[index] = TrieNode()
-
-            current_node = current_node.children[index]
-
-        current_node.is_end_of_word = True
-        current_node.original_word = original_word
-
-    def search_word(self, word):
-        word = word.lower()
-        current_node = self.root
-
-        for char in word:
-            index = self.get_index(char)
-
-            if current_node.children[index] is None:
-                return False
-
-            current_node = current_node.children[index]
-
-        return current_node.is_end_of_word
-
-    def search_prefix(self, prefix):
-        prefix = prefix.lower()
-        current_node = self.root
-
-        for char in prefix:
-            index = self.get_index(char)
-
-            if current_node.children[index] is None:
-                return []
-
-            current_node = current_node.children[index]
-
-        results = []
-        self.dfs(current_node, results)
-
-        return results
-
-    def dfs(self, node, results):
-        if node.is_end_of_word:
-            results.append(node.original_word)
-
-        for child_node in node.children:
-            if child_node is not None:
-                self.dfs(child_node, results)
-
-# FUNCTIONS
 def get_all_info(category):
     url = f"{base_url}{category}/"
     response = requests.get(url)
@@ -298,17 +146,19 @@ while True:
 
         compression_rate = (1 - (len(encoded_message) / (len(planets_string) * 8))) * 100
 
+        huffman_data = get_huffman_data()
+
         print("Mensagem com as informações dos planetas enviada com sucesso!")
         print("Mensagem enviada:")
         print(encoded_message)
         print("\nTabela de frequências:")
-        print(freq_map)
+        print(huffman_data["freq_map"])
         print("\nÁrvore de Huffman em pós ordem:")
-        print(huffman_post_list)
+        print(huffman_data["post_order"])
         print("\nÁrvore de Huffman em ordem simétrica:")
-        print(huffman_in_list)
+        print(huffman_data["in_order"])
         print("\nTabela de códigos binários:")
-        print(encode_map)
+        print(huffman_data["encode_map"])
         print(f"\nTaxa de compressão: {compression_rate:.2f}%")
 
         print()
